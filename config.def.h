b1cd44dc4b16107bb2aec0d03c934aee241dc57c
M. Nabil Adani <macroid48@gmail.com>
2019-10-30 23:32:52 +0700

    patch: dmenu-morecolor-20190922-4bf895b.diff
    https://tools.suckless.org/dmenu/patches/morecolor/

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#eeeeee", "#770000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
